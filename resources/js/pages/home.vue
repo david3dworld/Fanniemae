fc62c50927083b6e5c95e0cec7ed24a0b26a0e3e
DavidFullstackdev <star1995420@gmail.com>
2020-12-29 21:50:05 +0200

    inital commit

---
+++ resources/js/pages/home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: 'auth',
+
+  metaInfo () {
+    return { title: this.$t('home') }
+  },
+
+  data () {
+    return {
+      formValidate: {
+          name: '',
+          mail: '',
+          city: '',
+          gender: '',
+          interest: [],
+          date: '',
+          time: '',
+          desc: ''
+      },
+    }
+  },
+  methods: {
+    handleSubmit (name) {
+        this.$refs[name].validate((valid) => {
+            if (valid) {
+                this.$Message.success('Success!');
+            } else {
+                this.$Message.error('Fail!');
+            }
+        })
+    },
+    handleReset (name) {
+        this.$refs[name].resetFields();
+    }
+  }
+}
+</script>
---
+++ resources/js/plugins/index.js
@@ -0,0 +1,4 @@
+import './axios'
+import './fontawesome'
+import './iviewui'
+import 'bootstrap'
